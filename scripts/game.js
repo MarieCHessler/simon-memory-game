9b0a20dd8d56c575065dd783b666769de0c423e6
MarieCHessler <marie@hessler.se>
2022-03-22 13:37:39 +0000

    Initial commit

---
+++ scripts/game.js
@@ -0,0 +1,8 @@
+let game = {
+    score: 0,
+    currentGame: [], // Has empty array
+    playerMoves: [], // Has empty array
+    choices: ["button1", "button2", "button3", "button4"], // Has ID:s in array
+}
+
+module.exports = { game } // Curly braces needed to export more than one object and function from file

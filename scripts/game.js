7a5ef9b621e2b49ade6d29fe83ef914fad9ef794
MarieCHessler <marie@hessler.se>
2022-03-22 16:35:15 +0000

    Add lightsOn function

--- scripts/game.js
+++ scripts/game.js
@@ -17,11 +17,18 @@ function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
     // showTurns();
-}
+};
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 };
 
+function lightsOn(circ) { // Function will be called with the ID of one of the circles
+    document.getElementById(circ).classList.add("light"); // The class of light will be added to appropriate circle
+    setTimeout(() => { // Set timeout to remove light class after 400 milliseconds
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+};
+
 // Always add new objects and functions for export
-module.exports = { game, newGame, showScore, addTurn }; // Curly braces needed to export more than one object and function from file
+module.exports = { game, newGame, showScore, addTurn, lightsOn }; // Curly braces needed to export more than one object and function from file

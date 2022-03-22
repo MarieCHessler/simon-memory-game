5b1dbe18feb0f74f952c0b498ac0a0c5d4accad4
MarieCHessler <marie@hessler.se>
2022-03-22 14:56:38 +0000

    Add newGame function

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,18 @@ let game = {
     currentGame: [], // Has empty array
     playerMoves: [], // Has empty array
     choices: ["button1", "button2", "button3", "button4"], // Has ID:s in array
-}
+};
 
-module.exports = { game } // Curly braces needed to export more than one object and function from file
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+};
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+};
+
+// Always add new objects and functions for export
+module.exports = { game, newGame, showScore }; // Curly braces needed to export more than one object and function from file
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
 * @jest-environment jsdom
 */
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game"); // Always import new objects and functions
 
 
 beforeAll(() => {
@@ -28,5 +28,27 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42"; // Set the score to 42 on the DOM
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the the computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should display zero for the element with the id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
